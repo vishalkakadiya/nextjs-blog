89658cca43eea271ba1471ad9411de6c76179090
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-04-03 11:36:19 +0530

    Sample next.js app

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'BSBI';
-export const siteTitle = 'Next.js BSBI Website';
+const name = 'Next.js';
+export const siteTitle = 'Next.js Website';
 
 export default function Layout({ children, home }) {
     return (
